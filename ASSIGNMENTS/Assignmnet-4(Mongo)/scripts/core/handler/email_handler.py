3c47f332750993af3c88e6ea4ff2751703d3bb83
Surukati Asutosh Dora <78288601+SurukatiAsutoshDora@users.noreply.github.com>
2023-05-19 17:48:54 +0530

    Add files via upload

---
+++ ASSIGNMENTS/Assignmnet-4(Mongo)/scripts/core/handler/email_handler.py
@@ -0,0 +1,44 @@
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from scripts.schemas.inventory_schemas import Email
+from tabulate import tabulate
+import pandas as pd
+from scripts.logging.logs import logger
+from scripts.constants.email_constants import email_object
+from scripts.db.mongo import Item_handler
+
+
+class Email_handler:
+    """this class helps in building an smtp mail structure for sending to the recepient"""
+
+    def send_email(self, Email: Email):
+        sender_email = email_object.email_name
+        sender_password = email_object.email_password
+        receiver_email = Email.rec_email
+
+        message = MIMEMultipart()
+        message["From"] = sender_email
+        message["To"] = receiver_email
+        message["Subject"] = "Total Price Of The Inventory List"
+
+        inventory_object = Item_handler()
+        result = inventory_object.find_total()
+        get_list = inventory_object.fetch()
+        df = pd.DataFrame(get_list)
+        table = tabulate(df, headers='keys', tablefmt='table')
+        body = str(result)
+        message.attach(MIMEText(("THESE ARE THE ITEMS IN YOUR INVENTORY :\n" +table + "\n Total amount : " + body), "plain"))
+        try:
+
+            server = smtplib.SMTP("smtp.gmail.com", 587)
+            server.starttls()
+            server.login(sender_email, sender_password)
+            server.send_message(message)
+            server.quit()
+            logger.info({"message": "Email sent successfully"})
+            return {"message": "Email sent successfully"}
+
+        except Exception as e:
+            logger.error({"message": str(e)})
+            return {"message": str(e)}
